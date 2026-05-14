3b14c1984a6de752e2d708e849eaef4c58533b29
Renato Davis <renatodavis@hotmail.com>
2026-05-14 00:01:50 -0300

    Implementação Spec para issue #1: Criar um projeto calculadora
    Co-authored-by: Renato Davis <renatodavis@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
+```javascript
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.spec.ts'],
-  clearMocks: true,
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/']
 };
+```
